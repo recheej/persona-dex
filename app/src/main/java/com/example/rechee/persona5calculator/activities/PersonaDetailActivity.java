a8f050b7f6dba79f126b2bddae19ef7d5d052c1f
Rechee Jozil <recheejozil@hotmail.com>
2017-07-09 12:29:49 -0400

    Added new persona detail activity

---
+++ app/src/main/java/com/example/rechee/persona5calculator/activities/PersonaDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.rechee.persona5calculator.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rechee.persona5calculator.R;
+
+public class PersonaDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_persona_detail);
+    }
+}
