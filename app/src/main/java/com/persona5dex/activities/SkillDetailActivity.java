334ead01961b0bfefbb84187ba348bdbab90d8d4
Rechee Jozil <recheejozil/@hotmail.com>
2017-12-30 18:09:20 -0500

    Added skill detail activity

---
+++ app/src/main/java/com/persona5dex/activities/SkillDetailActivity.java
@@ -0,0 +1,13 @@
+package com.persona5dex.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SkillDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skill_detail);
+    }
+}
