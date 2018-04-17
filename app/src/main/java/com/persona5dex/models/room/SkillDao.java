4b3d9aca9b21eb9e32430ca6ba2bcd7a47d46e68
Rechee Jozil <recheejozil/@hotmail.com>
2018-04-16 22:44:59 -0700

    Made skill dao transaction for main list persona

--- app/src/main/java/com/persona5dex/models/room/SkillDao.java
+++ app/src/main/java/com/persona5dex/models/room/SkillDao.java
@@ -3,6 +3,7 @@ package com.persona5dex.models.room;
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Transaction;
 
 import com.persona5dex.models.MainListPersona;
 import com.persona5dex.models.PersonaDetailSkill;
@@ -27,6 +28,7 @@ public interface SkillDao {
     )
     LiveData<List<PersonaDetailSkill>> getPersonaSkills(int personaID);
 
+    @Transaction
     @Query(" select id, name, arcanaName, arcana, level, rare, dlc from personaSkills\n" +
             "inner join personas on personas.id = personaSkills.persona_id\n" +
             "where personaSkills.skill_id = :skillID\n" +
