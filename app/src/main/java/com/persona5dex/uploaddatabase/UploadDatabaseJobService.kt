5e08e3bb45dbc4138330293ee933a43b90bb7196
Rechee Jozil <recheejozil/@hotmail.com>
2018-06-06 20:39:58 -0700

    Replaced workmanager with job dispatcher

--- app/src/main/java/com/persona5dex/uploaddatabase/UploadDatabaseJobService.kt
+++ app/src/main/java/com/persona5dex/uploaddatabase/UploadDatabaseJobService.kt
@@ -1,31 +1,42 @@
 package com.persona5dex.uploaddatabase
 
 import android.net.Uri
-import androidx.work.Worker
+import com.firebase.jobdispatcher.JobParameters
+import com.firebase.jobdispatcher.JobService
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.UploadTask
 import com.persona5dex.models.room.PersonaDatabase
 import java.io.File
 
-const val DATABASES_PATH = "databases"
+private const val DATABASES_PATH = "databases"
 
-class UploadDatabaseWorker: Worker() {
-    override fun doWork(): WorkerResult {
+public const val TAG = "job-service-upload-db"
+
+class UploadDatabaseJobService: JobService() {
+    override fun onStopJob(job: JobParameters?): Boolean {
+        return false
+    }
+
+    override fun onStartJob(job: JobParameters?): Boolean {
         val firebaseStorage: FirebaseStorage = FirebaseStorage.getInstance()
         val storageReference = firebaseStorage.reference
 
         val databaseFile: File = PersonaDatabase.getPersonaDatabase(applicationContext).databaseFile
         val databaseUri = Uri.fromFile(databaseFile)
 
-        val uploadpath = "$DATABASES_PATH/persona-db-${System.currentTimeMillis()}.db"
-        val storageRef: StorageReference = storageReference.child(uploadpath)
+        val uploadPath = "$DATABASES_PATH/persona-db-${System.currentTimeMillis()}.db"
+        val storageRef: StorageReference = storageReference.child(uploadPath)
 
         val uploadTask: UploadTask = storageRef.putFile(databaseUri)
+        uploadTask.addOnSuccessListener {
+            jobFinished(job!!, false)
+        }
+
         uploadTask.addOnFailureListener({
-            //how to handle??
+            jobFinished(job!!, true)
         })
 
-        return WorkerResult.SUCCESS
+        return true
     }
 }
--- app/src/main/java/com/persona5dex/uploaddatabase/UploadDbPreferenceDialogFragment.kt
+++ app/src/main/java/com/persona5dex/uploaddatabase/UploadDbPreferenceDialogFragment.kt
@@ -1,13 +1,12 @@
 package com.persona5dex.uploaddatabase
 
 import android.content.DialogInterface
-import android.support.v7.preference.DialogPreference
-import android.support.v7.preference.PreferenceDialogFragmentCompat
 import android.os.Bundle
+import android.support.v7.preference.DialogPreference
 import android.support.v7.preference.Preference
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
-
+import android.support.v7.preference.PreferenceDialogFragmentCompat
+import com.firebase.jobdispatcher.FirebaseJobDispatcher
+import com.firebase.jobdispatcher.GooglePlayDriver
 
 class UploadDbPreferenceDialogFragment: PreferenceDialogFragmentCompat() {
 
@@ -31,9 +30,14 @@ class UploadDbPreferenceDialogFragment: PreferenceDialogFragmentCompat() {
     override fun onClick(dialog: DialogInterface?, which: Int) {
         val okButtonClicked = which == -1
         if(okButtonClicked) {
-            val uploadDbWork = OneTimeWorkRequest.Builder(UploadDatabaseWorker::class.java)
+            val dispatcher = FirebaseJobDispatcher(GooglePlayDriver(context))
+
+            val myJob = dispatcher.newJobBuilder()
+                    .setService(UploadDatabaseJobService::class.java) // the JobService that will be called
+                    .setTag(TAG)        // uniquely identifies the job
                     .build()
-            WorkManager.getInstance().enqueue(uploadDbWork)
+
+            dispatcher.mustSchedule(myJob)
         }
     }
 }
