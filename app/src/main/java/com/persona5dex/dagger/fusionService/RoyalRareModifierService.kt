7e20775582a312635319663fb624a9a0f95533bf
Rechee <recheejozil@hotmail.com>
2020-03-12 19:14:20 -0700

    created rare parser for royal

---
+++ app/src/main/java/com/persona5dex/dagger/fusionService/RoyalRareModifierService.kt
@@ -0,0 +1,88 @@
+package com.persona5dex.dagger.fusionService
+
+import android.content.Context
+import android.util.JsonReader
+import com.persona5dex.ArcanaNameProvider
+import com.persona5dex.R
+import com.persona5dex.models.Enumerations.Arcana
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.io.InputStreamReader
+import javax.inject.Named
+
+class RoyalRareModifierService constructor(
+        @Named("applicationContext") context: Context,
+        val arcanaNameProvider: ArcanaNameProvider
+) : PersonaRareFileModifierService(context) {
+
+    override fun getFileRes(): Int = R.raw.royal_rare_combos
+
+    override suspend fun parseFile(fileInputStream: InputStream): RarePersonaModificationManager =
+            withContext(Dispatchers.IO) {
+                JsonReader(InputStreamReader(fileInputStream, "UTF-8")).run {
+                    use {
+
+                        val rarePersonaMap = mutableMapOf<Arcana, Array<Int>>()
+                        beginObject()
+
+                        val rarePersonas = getRarePersonas()
+                        val arcanas = getArcanas()
+
+                        addToRarePersonaMap(rarePersonaMap, arcanas, rarePersonas.size)
+
+                        endObject()
+
+                        RarePersonaModificationManager(rarePersonaMap, rarePersonas)
+                    }
+                }
+            }
+
+    private fun JsonReader.addToRarePersonaMap(rarePersonaMap: MutableMap<Arcana, Array<Int>>, arcanas: Array<Arcana>, personaCount: Int) {
+        check(nextName() == "table")
+        beginArray()
+
+        var index = 0
+        while (hasNext()) {
+            beginArray()
+
+            val arcana = arcanas[index]
+
+            var personaIndex = 0
+            while (hasNext()) {
+                rarePersonaMap.getOrPut(arcana) {
+                    IntArray(personaCount).toTypedArray()
+                }[personaIndex] = nextInt()
+                personaIndex++
+            }
+
+            endArray()
+            index++
+        }
+        endArray()
+    }
+
+    private fun JsonReader.getArcanas(): Array<Arcana> {
+        check(nextName() == "races")
+
+        val arcanas = mutableListOf<Arcana>()
+        beginArray()
+        while (hasNext()) {
+            arcanas.add(arcanaNameProvider.getArcanaForEnglishNameOrThrow(nextString()))
+        }
+        endArray()
+        return arcanas.toTypedArray()
+    }
+
+    private fun JsonReader.getRarePersonas(): Array<String> {
+        check(nextName() == "elems")
+        beginArray()
+
+        val rarePersonas = mutableListOf<String>()
+        while (hasNext()) {
+            rarePersonas.add(nextString())
+        }
+        endArray()
+        return rarePersonas.toTypedArray()
+    }
+}
---
+++ app/src/test/java/com/persona5dex/dagger/fusionService/PersonRoyalRareModifierServiceTest.kt
@@ -0,0 +1,50 @@
+package com.persona5dex.dagger.fusionService
+
+import android.content.Context
+import android.os.Build
+import com.persona5dex.ArcanaNameProvider
+import com.persona5dex.models.Enumerations.Arcana
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = "src/main/AndroidManifest.xml", sdk = [Build.VERSION_CODES.O])
+class PersonRoyalRareModifierServiceTest {
+
+    private lateinit var arcanaNameProvider: ArcanaNameProvider
+    private lateinit var context: Context
+
+    private lateinit var modifierService: RoyalRareModifierService
+
+    @Before
+    fun setup() {
+        context = RuntimeEnvironment.application
+        arcanaNameProvider = ArcanaNameProvider(context)
+
+        modifierService = RoyalRareModifierService(context, arcanaNameProvider)
+    }
+
+    @Test
+    fun `getRareModifierManager() returns valid manager`() {
+        runBlocking {
+            val manager = modifierService.getRareModifierManager()
+            val modifier = manager.getModifier(Arcana.FOOL, "Koh-i-Noor")
+            Assert.assertEquals(1, modifier)
+        }
+    }
+
+    @Test
+    fun `getRareModifierManager() returns valid manager for hanged man`() {
+        runBlocking {
+            val manager = modifierService.getRareModifierManager()
+            val modifier = manager.getModifier(Arcana.HANGED_MAN, "Koh-i-Noor")
+            Assert.assertEquals(-1, modifier)
+        }
+    }
+}
