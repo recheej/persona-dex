410d6794153a3c2084bb0e7beccc8994f5ef594c
Rechee <recheejozil@hotmail.com>
2020-03-17 00:36:55 -0700

    removing method trace

--- app/src/main/java/com/persona5dex/PersonaFilters.kt
+++ app/src/main/java/com/persona5dex/PersonaFilters.kt
@@ -12,13 +12,11 @@ fun <T : GameTypePersona> List<T>.filterGameType(gameType: GameType): List<T> {
     return if (gameType == GameType.BASE) {
         basePersonas
     } else {
-        Debug.startMethodTracing("demo.trace")
         val personasForGameType = filter { it.gameType == gameType }
         val personasForGameNameSet = personasForGameType.map { it.name }.toSet()
         val allGameTypePersonas = basePersonas + personasForGameType
         val result = allGameTypePersonas
                 .filterNot { it.gameType == GameType.BASE && personasForGameNameSet.contains(it.name) }
-        Debug.stopMethodTracing()
         result
     }
 }
