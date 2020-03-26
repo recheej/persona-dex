1049924c03da21b7d35e712df4635726b052f3ea
Rechee <recheejozil@hotmail.com>
2020-03-25 19:53:15 -0700

    notify about dlc settings

--- app/src/main/java/com/persona5dex/activities/MainActivity.java
+++ app/src/main/java/com/persona5dex/activities/MainActivity.java
@@ -4,22 +4,18 @@ import android.app.SearchManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.text.SpannableString;
-import android.text.method.LinkMovementMethod;
-import android.text.util.Linkify;
-import android.view.ContextThemeWrapper;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.StringRes;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
@@ -37,7 +33,7 @@ import com.persona5dex.fragments.PersonaSkillsFragment;
 import com.persona5dex.jobs.PersonaJobCreator;
 import com.persona5dex.models.Enumerations.SearchResultType;
 import com.persona5dex.models.GameType;
-import com.persona5dex.onboarding.OnboardingActivity;
+import com.persona5dex.onboarding.OnboardingActivityResultContract;
 import com.persona5dex.repositories.MainPersonaRepository;
 
 import javax.inject.Inject;
@@ -68,14 +64,16 @@ public class MainActivity extends BaseActivity {
     private Button switchGameButton;
     private TextView currentGameTextView;
 
+    ActivityResultLauncher<Void> onboardingResultLauncher = prepareCall(new OnboardingActivityResultContract(this),
+            result -> Toast.makeText(this, R.string.dlc_settings_hint, Toast.LENGTH_LONG).show());
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         final boolean onboardingComplete = defaultSharedPreferences.getBoolean(Constants.SHARED_PREF_ONBOARDING_COMPLETE, false);
         if(!onboardingComplete) {
-            final Intent intent = new Intent(this, OnboardingActivity.class);
-            startActivity(intent);
+            onboardingResultLauncher.launch(null);
         }
 
         setContentView(R.layout.activity_main);
